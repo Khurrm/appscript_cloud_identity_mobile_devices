4f3d27555018e84c75734f63c27f04138d2276be
Khurram <khurramworkspace@gmail.com>
2023-02-21 23:50:10 +0100

    Adding the URL fetch to test it out.

--- script_scriptapp_oauth_user.js
+++ script_scriptapp_oauth_user.js
@@ -3,5 +3,14 @@ function listMobileDevices_CloudIdentity() {
   var token = ScriptApp.getOAuthToken();
   console.log(token)
 
-
+  /*var response = UrlFetchApp.fetch(url, {
+    headers: {
+      Authorization: 'Bearer ' + token
+    }
+  });
+  var result = JSON.parse(response.getContentText());
+  result.siteEntry.forEach(function(siteEntry) {
+    Logger.log(siteEntry.siteUrl);
+  });
+*/
 }
